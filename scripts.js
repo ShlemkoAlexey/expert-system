a4bc2b4bb51a437610228145a5f117ab8fa9d4a2
ShlemkoAlexey <shlemko.alexey@gmail.com>
2016-05-23 12:30:48 +0300

    data loading alpha

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ var selectFieldItem = $(".city-options-list li");
 
 inputField.keyup(function (event) {
   if (inputField.val().length > 0) {
-      showCityListForSelect(citiesArray, inputField.val(), inputField, selectField);
+    showCityListForSelect(citiesArray, inputField.val(), inputField, selectField);
   }else {
     selectField.empty();
   }
@@ -18,20 +18,28 @@ $(document).on("click", ".city-options-list li", function(){
 });
 
 $(document).ready(function(){
-  $.getJSON("http://178.62.229.113/cities")
-      .done(function(data){
-        for (var i = 0; i < data.length; i++){
-          citiesArray.push(data[i]);
-        }
-        console.log("city data recieved");
-      })
-      .fail(function(){
-        console.log("city list load failed");
-      })
-
-
-
+  loadCityList();
+  loadAdverts("Amsterdam");
 });
+
+$(".filter-button").click(function(){
+  loadAdverts(inputField.val());
+});
+function loadAdverts(city){
+  $.getJSON("http://178.62.229.113/filter/include/city/"+city)
+    .done(function(data){
+      console.log("adverts data loaded");
+      $(".results-block").empty();
+      for (var i = 0; i < data.length; i++) {
+        $(".results-block").append(createAdvertFrame(data[i].url, 'http://wooden-houses.kiev.ua/wp-content/uploads/2014/06/dom-profilirovannyi-brus.jpg', data[i].city, data[i].actual_price, data[i].predicted_price, data[i].price_diff, data[i].has_garden, data[i].has_garage));
+      }
+    })
+    .fail(function(){
+      console.log("advert data load failed");
+    });
+}
+
+
 
 function showCityListForSelect(array, string, input, list){
   var arrayForOutput = [];
@@ -49,4 +57,43 @@ function showCityListForSelect(array, string, input, list){
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function createAdvertFrame(url, image_url, location, actual_price, predicted_price, price_diff, has_garden, has_garage){
+  predicted_price = predicted_price.toFixed();
+  price_diff = price_diff.toFixed();
+
+  if (has_garden == 0){
+    has_garden = "No"
+  }else{
+    has_garden = "Yes"
+  }
+
+  if (has_garage == 0){
+    has_garage = "No"
+  }else{
+    has_garage = "Yes"
+  }
+
+  var price_comparison;
+  if(price_diff >=0){
+    price_comparison = "overrated";
+  }else{
+    price_comparison = "underrated";
+  }
+
+  return '<div class="result"><div class="row"><div class="col-xs-2"><img src="'+image_url+'" alt="result-image" width="100%"/></div><div class="col-xs-10"><p>Location: '+location+'</p><p>Actual price: '+actual_price+' €<br>Predicted price: '+predicted_price+' €<br>Price difference : '+price_diff+' € ('+price_comparison+')<br><br>Garden: ' + has_garden + '<br>Garage: ' + has_garage + '</p><a href="'+url+'" target="_blank">Go to advert...</a></div></div></div>';
+}
+
+function loadCityList(){
+  $.getJSON("http://178.62.229.113/cities")
+  .done(function(data){
+    for (var i = 0; i < data.length; i++){
+      citiesArray.push(data[i]);
+    }
+    console.log("city list loaded");
+  })
+  .fail(function(){
+    console.log("city list load failed");
+  });
 }
