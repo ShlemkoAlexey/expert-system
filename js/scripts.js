169bb19f0a1b47db396e45eb4e22afbaaa560a9b
ShlemkoAlexey <shlemko.alexey@gmail.com>
2016-05-26 16:41:34 +0300

    new advert block release

--- js/scripts.js
+++ js/scripts.js
@@ -25,10 +25,9 @@ $(document).on("click", ".city-options-list li", function(){
 
 $(document).ready(function(){
   loadCityList();
-  //loadAdverts("http://178.62.229.113/results/1");
-  //Statements.requestAdress = 'http://178.62.229.113/results/';
-  //createPaginator(Statements.currentPage, Statements.totalPages);
-  $('.result-prices p').tooltip();
+  loadAdverts("http://178.62.229.113/results/1");
+  Statements.requestAdress = 'http://178.62.229.113/results/';
+  createPaginator(Statements.currentPage, Statements.totalPages);
 });
 
 $(".filter-button").click(function(){
@@ -43,10 +42,11 @@ function loadAdverts(requestAdress){
     console.log("adverts data loaded");
     $(".results-block").empty();
     for (var i = 0; i < data.results.length; i++) {
-      $(".results-block").append(createAdvertFrame(data.results[i].url, data.results[i].preview_url, data.results[i].city, data.results[i].actual_price, data.results[i].predicted_price, data.results[i].price_diff, data.results[i].has_garden, data.results[i].has_garage));
+      $(".results-block").append(createAdvertFrame(data.results[i].url, data.results[i].preview_url, data.results[i].city, data.results[i].actual_price, data.results[i].predicted_price, data.results[i].price_diff, data.results[i].has_garden, data.results[i].has_garage, data.results[i].house_type, data.results[i].rooms_count, data.results[i].bedrooms_count, data.results[i].description, data.results[i].area));
     }
     Statements.totalPages = data.pages_count;
     createPaginator(Statements.currentPage, Statements.totalPages);
+    $('.result-prices p').tooltip();
   })
   .fail(function(){
     console.log("advert data load failed");
@@ -74,37 +74,42 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
-function createAdvertFrame(url, image_url, location, actual_price, predicted_price, price_diff, has_garden, has_garage){
+function createAdvertFrame(url, image_url, location, actual_price, predicted_price, price_diff, has_garden, has_garage, type, rooms, bedrooms, description, area){
   predicted_price = predicted_price.toFixed();
   price_diff = price_diff.toFixed();
 
   if (has_garden == 0){
-    has_garden = "No"
+    has_garden = "<i class='mdi mdi-close'></i>"
   }else{
-    has_garden = "Yes"
+    has_garden = "<i class='mdi mdi-check'></i>"
   }
 
   if (has_garage == 0){
-    has_garage = "No"
+    has_garage = "<i class='mdi mdi-close'></i>"
   }else{
-    has_garage = "Yes"
+    has_garage = "<i class='mdi mdi-check'></i>"
   }
 
   var price_comparison;
   if(price_diff >=0){
-    price_comparison = "overrated";
+    price_comparison = '<p data-toggle="tooltip" data-placement="bottom" title="Overrated"><i class="mdi mdi-arrow-up-bold"></i> <span style="color:red">'+price_diff+'€</span></p>';
   }else{
-    price_comparison = "underrated";
+    price_comparison = '<p data-toggle="tooltip" data-placement="bottom" title="Underrated"><i class="mdi mdi-arrow-down-bold"></i> <span style="color:green">'+(price_diff*(-1))+'€</span></p>';
   }
 
-  return '<div class="result"><div class="row"><div class="col-xs-2"><img src="'+image_url+'" alt="result-image" width="100%"/></div><div class="col-xs-10"><p>Location: '+location+'</p><p>Actual price: '+actual_price+' €<br>Predicted price: '+predicted_price+' €<br>Price difference : '+price_diff+' € ('+price_comparison+')<br><br>Garden: ' + has_garden + '<br>Garage: ' + has_garage + '</p><a href="'+url+'" target="_blank">Go to advert...</a></div></div></div>';
+  if (type == 0) {
+    type = "House"
+  }else {
+    type = "Apartment"
+  }
+  return '<div class="result">  <div class="row">  <div class="col-xs-2">  <a href="'+url+'" target="_blank"><img src="'+image_url+'" alt="result-image" class="result-image"/></a>  </div>  <div class="col-xs-10">  <a class="result-city-name" href="'+url+'" target="_blank">'+capitalizeFirstLetter(location)+'</p></a>  <div class="row">  <div class="col-xs-8">  <div class="row">  <div class="col-xs-5 result-data">  <p>  Type: '+type+'  </p>  <p>  Rooms: '+rooms+' ('+bedrooms+' bedrooms)  </p>  <p>  Area: '+area+' m<sup>3</sup>  </p>  </div>  <div class="col-xs-7 result-options">  <p>  Garden:'+has_garden+'  </p>  <p>  Garage: '+has_garage+'  </p>  </div>  </div>  <p class="result-description">  '+cutDescription(description)+'  </p>  <a target="_blank" href="'+url+'" class="result-link">  Go to advert...  </a>  </div>  <div class="col-xs-4 result-prices">  <p>  actual:<span>'+actual_price+'€</span>  </p>  <p>  predicted:<span>'+predicted_price+'€</span>  </p>  '+price_comparison+'  </div>  </div>  </div>  </div>  </div>';
 }
 
 function loadCityList(){
   $.getJSON("http://178.62.229.113/cities")
   .done(function(data){
     for (var i = 0; i < data.length; i++){
-      citiesArray.push(data[i]);
+      citiesArray.push(capitalizeFirstLetter(data[i]));
     }
     console.log("city list loaded");
   })
@@ -155,6 +160,8 @@ function bindEventsToPaginator(){
   $(".paginator ul li").on("click", function(){
     $(document).scrollTop(0);
   })
+}
 
-
+function cutDescription(string){
+    return string.substr(0,400) + "...";
 }
