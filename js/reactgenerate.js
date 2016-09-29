da00334a3abba099dd1279e605ef7954f10f8f24
ShlemkoAlexey <shlemko.alexey@gmail.com>
2016-09-29 17:26:46 +0300

    footer generated by reactjs now

---
+++ js/reactgenerate.js
@@ -0,0 +1,48 @@
+  ReactDOM.render(React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "p",
+      null,
+      React.createElement(
+        "b",
+        null,
+        "Back-end: Illia B."
+      )
+    ),
+    React.createElement(
+      "p",
+      null,
+      React.createElement(
+        "b",
+        null,
+        "Front-end: Alexey S."
+      )
+    ),
+    React.createElement(
+      "p",
+      null,
+      React.createElement(
+        "b",
+        null,
+        "Contact us: ",
+        React.createElement(
+          "a",
+          { href: "mailto:geen.makelaar@gmail.com" },
+          "geen.makelaar@gmail.com"
+        )
+      )
+    ),
+    React.createElement(
+      "p",
+      null,
+      React.createElement(
+        "b",
+        null,
+        "© 2016 All rights reserved"
+      )
+    )
+  ), document.getElementById('footer'));
+
+
+  console.log("log!");
